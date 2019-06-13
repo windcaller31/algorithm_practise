2ffaeae46c7a8c5f5294c06c1b1f9da70a4ae06a
limengtong <limengtong@iyunxiao.com>
2019-06-13 18:59:40 +0800

    change logic

--- leetcode/src/Recursion/FullPermutation.java
+++ leetcode/src/Recursion/FullPermutation.java
@@ -3,48 +3,30 @@ package Recursion;
 import java.util.HashSet;
 import java.util.Set;
 
+
+// f(1,2,...n) = {最后一位是 1, f(n-1)...
+
 public class FullPermutation {
 	public static void main(String args[]){
 		char[] chars = {'1','2','3'};
-		per(chars,0);
+		per(chars,3,3);
 	}
-
-	static Set<String> s = new HashSet<String>();
-	public static void per(char[] data,int begin){
-		int length = data.length;
-		if(begin==length){
-			s.add(String.valueOf(data));
-			System.out.println(data);
-		}
-		for(int i =begin;i<length;i++){
-			// if(!isUnique(data,begin,i)){
-			if(!isUnique(data.toString())){
-				swap(data,begin,i);
-				per(data, begin + 1);
-				swap(data,begin,i);
+	public static void per(char[] data,int n,int k){
+		if(k==1){
+			for(int i =0;i<n;i++){
+				System.out.print(data[i]);
 			}
+			System.out.println(" ");
+		}
+		for(int i =0;i<k;i++){
+			swap(data,i,k-1);
+			per(data,n,k-1);
+			swap(data,i,k-1);
 		}
 	}
-
-	private static boolean isUnique(String data){
-		if(s.contains(String.valueOf(data))){
-			return true;
-		}
-		return false;
-	}
-
-	//另类比较唯一性
-//	private static boolean isUnique(char[] data, int begin, int end){
-//        for(int i = begin; i < end; i++){
-//        	if(data[i] == data[end]){
-//            	return true;
-//            }
-//        }
-//        return false;
-//    }
 
 	private static void swap(char[] nums,int i,int j){
-		System.out.println(i+"  "+j);
+//		System.out.println(i+"  "+j);
 		char temp = nums[i];
 		nums[i] = nums[j];
 		nums[j] = temp;
