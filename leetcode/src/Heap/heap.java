a3a11152434c84dd94594ac56d66e6c31f1d5dbf
limengtong <limengtong@iyunxiao.com>
2019-06-14 19:57:57 +0800

    堆 和 堆排序

---
+++ leetcode/src/Heap/heap.java
@@ -0,0 +1,60 @@
+package Heap;
+
+public class heap {
+	private int[] a;  //数组下标从1开始
+	private int n; //堆中可以存储的最多数据个数
+	private int count; //堆中已有数据个数
+	
+	public heap(int capacity){
+		a = new int[capacity+1];
+		n = capacity;
+		count = 0;
+	}
+	
+	//新增元素
+	public void insert(int data){
+		if(count>=n) return; // 堆满了
+		++count;
+		a[count] = data;
+		int i = count;
+		while(i/2>=0&&a[i]>a[i/2]){  // 与根节点比较
+			swap(a,i,i/2);
+			i = i/2;
+		}
+	}
+	
+	//删除元素(删除最大元素堆顶)
+	public void removeMax(){ 
+		if(count==0){  // 堆中无元素
+			return;
+		}
+		//将堆底元素换至堆顶从新开始堆化,避免数组空洞
+		a[1] = a[count];
+		count--;
+		heapify(a,count,1);
+	}
+	
+	//自上而下堆化 堆顶元素向下堆化，找到合适位置放进去
+	public void heapify(int[] a,int n, int i){
+		while(true){
+			int maxPos = i;
+			if(i*2<=n&&a[i]>a[i*2]){
+				maxPos = i*2;
+			}
+			if(i*2+1<=n&&a[i]>a[i*2+1]){
+				maxPos = i*2+1;
+			}
+			if(maxPos==i){//未进行过交换
+				break;
+			}
+			swap(a,i,maxPos);//换过后继续堆化
+			i = maxPos;
+		}
+	}
+	
+	public void swap(int[] a, int i, int j){ 
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+}
---
+++ leetcode/src/sort/HeapSort.java
@@ -0,0 +1,56 @@
+package sort;
+
+import util.Printer;
+
+//由于从0开始则 节点i 父节点(i-1)/2  左子2*i+1 右子2*i+2
+//如果从1开始则 节点i 父节点(i)/2  左子2*i 右子2*i+1 
+public class HeapSort {
+	public static void main(String args[]){
+		int[] a={3,4,6,1,2,9,8,7};
+		sort(a);
+	}
+	
+	public static void sort(int[] a){
+		//建堆
+		buildHeap(a,a.length-1);
+		int k = a.length-1;
+		//大顶堆，把大元素放到第最后一位k，然后k--剩下的位堆化，第二大的就到堆顶了，重复这一轮回
+		while(k>0){
+			swap(a,0,k); 
+			k--;
+			buildHeap(a ,k);
+		}
+		System.out.print(Printer.printArray(a));
+	}
+	
+	public static void buildHeap(int[]a , int n){
+		for(int i = (n-1)/2;i>=0;--i){  //传进来的数组a，看成完全二叉树。从非叶节点开始进行堆化建堆
+			heapify(a,n,i);
+		}
+	}
+	
+	//下至上堆化 建堆
+	public static void heapify(int[] a,int n, int i){
+		while(true){
+			int maxPos = i;
+			if(i*2+1<=n&&a[maxPos]<a[i*2+1]){
+				maxPos = i*2+1;
+			}
+			if(i*2+2<=n&&a[maxPos]<a[i*2+2]){
+				maxPos = i*2+2;
+			}
+			if(maxPos==i){
+				break;
+			}
+			swap(a,i,maxPos);
+			i = maxPos;
+		}
+	}
+	
+	public static void swap(int[] a,int i, int j){
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+	
+}
